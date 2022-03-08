c2f9538e53980513602a0822bc6bbeee500df01f
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2022-03-08 23:59:22 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$(window).on("load", function() {
+  let slideIndex = $(".slide.active").index();
+  const slideLen = $(".slide").length;
+
+  function slideShow() {
+    $(".slide").removeClass("active").eq(slideIndex).addClass("active");
+    if(slideIndex == slideLen-1) {
+      slideIndex = 0;
+    }
+    else {
+      slideIndex++;
+    }
+    setTimeout(slideShow, 5000);
+  }
+  slideShow();
+});
+
+// Filter
+$(document).ready(function() {
+
+  peopleFilter($(".filter-btn.active").attr("data-target"));
+
+  $(".filter-btn").click(function() {
+    if(!$(this).hasClass("active")) {
+      peopleFilter($(this).attr("data-target"));
+    };
+  });
+
+  function peopleFilter(target) {
+    $(".filter-btn").removeClass("active");
+    $(".filter-btn[data-target='"+target+"']").addClass("active");
+    $(".people-item").hide();
+    $(".people-item[data-category='"+target+"']").fadeIn();
+  }
+
+  const wHeight = $(window).height();
+  $(".gallery-popup img").css("max-height", wHeight + "px");
+
+});
+
