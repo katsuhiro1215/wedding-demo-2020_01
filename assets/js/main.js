367873a414d5ad6867b9bdbba4394424e7fe6219
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2022-03-09 11:58:00 +0900

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-$(window).on("load", function() {
+$(window).on("load", function () {
   let slideIndex = $(".slide.active").index();
   const slideLen = $(".slide").length;
 
   function slideShow() {
     $(".slide").removeClass("active").eq(slideIndex).addClass("active");
-    if(slideIndex == slideLen-1) {
+    if (slideIndex == slideLen - 1) {
       slideIndex = 0;
     }
     else {
@@ -17,26 +17,99 @@ $(window).on("load", function() {
   slideShow();
 });
 
-// Filter
-$(document).ready(function() {
+$(document).ready(function () {
 
+  // Menu Toggle
+  $(".menu-toggle").click(function() {
+    $(".header .header-nav").slideToggle();
+  });
+  $(".header .header-nav ul li a").click(function() {
+    if($(window).width() < 768) {
+      $(".header .header-nav").slideToggle();
+    }
+  })
+
+  // Fixed Header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".header").addClass("fixed");
+    } else {
+      $(".header").removeClass("fixed");
+    }
+  });
+
+  // scrollIt
+  $.scrollIt({
+    topOffset: -50
+  });
+
+  // Filter
   peopleFilter($(".filter-btn.active").attr("data-target"));
 
-  $(".filter-btn").click(function() {
-    if(!$(this).hasClass("active")) {
+  $(".filter-btn").click(function () {
+    if (!$(this).hasClass("active")) {
       peopleFilter($(this).attr("data-target"));
     };
   });
 
   function peopleFilter(target) {
     $(".filter-btn").removeClass("active");
-    $(".filter-btn[data-target='"+target+"']").addClass("active");
+    $(".filter-btn[data-target='" + target + "']").addClass("active");
     $(".people-item").hide();
-    $(".people-item[data-category='"+target+"']").fadeIn();
+    $(".people-item[data-category='" + target + "']").fadeIn();
   }
 
   const wHeight = $(window).height();
-  $(".gallery-popup img").css("max-height", wHeight + "px");
+  $(".gallery-popup .gp-img").css("max-height", wHeight + "px");
+
+  let itemIndex = 0;
+  const totalGalleryItems = $(".gallery-item").length;
+
+  $(".gallery-item").click(function () {
+    itemIndex = $(this).index();
+    $(".gallery-popup").addClass("open");
+    $(".gallery-popup .gp-img").hide();
+    gpSlideShow();
+  });
+
+  $(".gp-controls .next").click(function () {
+    if (itemIndex == totalGalleryItems - 1) {
+      itemIndex = 0;
+    } else {
+      itemIndex++;
+    }
+    $(".gallery-popup .gp-img").fadeOut(function () {
+      gpSlideShow();
+    });
+  });
+
+  $(".gp-controls .prev").click(function () {
+    if (itemIndex === 0) {
+      itemIndex = totalGalleryItems - 1;
+    } else {
+      itemIndex--;
+    }
+    $(".gallery-popup .gp-img").fadeOut(function () {
+      gpSlideShow();
+    });
+  });
+
+  function gpSlideShow() {
+    const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+    $(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
+    $(".gp-counter").text((itemIndex + 1) + "/" + totalGalleryItems);
+  }
+
+  // Close Gallery Popup
+  $(".gp-close").click(function () {
+    $(".gallery-popup").removeClass("open");
+  });
+
+  $(".gallery-popup").click(function (event) {
+    if ($(event.target).hasClass("open")) {
+      $(".gallery-popup").removeClass("open");
+    }
+  });
 
 });
 
